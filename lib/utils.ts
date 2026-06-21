8651e7412896077cfd141eb355fad61d1a24c368
pixelgamesite-maker <pixelgamesite@gmail.com>
2026-06-21 18:15:40 +0100

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+export { formatEther } from 'viem';
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
